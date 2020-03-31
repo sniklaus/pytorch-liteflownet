5943fcc5fdbe1ff434a8ded4fef19d29e249cf49
Simon Niklaus <simon.niklaus@outlook.com>
2020-03-31 09:54:44 -0700

    no message

--- download.bash
+++ download.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget --timestamping http://content.sniklaus.com/github/pytorch-liteflownet/network-default.pytorch
-wget --timestamping http://content.sniklaus.com/github/pytorch-liteflownet/network-kitti.pytorch
-wget --timestamping http://content.sniklaus.com/github/pytorch-liteflownet/network-sintel.pytorch
+wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-liteflownet/network-default.pytorch
+wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-liteflownet/network-kitti.pytorch
+wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-liteflownet/network-sintel.pytorch
